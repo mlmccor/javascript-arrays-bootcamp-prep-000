a4aaf8d066138105446d31d1ead45cb7e5acd9f8
Matthew McCormack <mmccormack1182@gmail.com>
2018-04-13 23:25:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,3 +35,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
   return array
 }
+function removeElementFromEndOfArray (array) {
+  var newarray= array.slice(0,array.length -1)
+  return newarray
+}
